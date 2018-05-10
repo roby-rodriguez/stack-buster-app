2d7cae67c865a8059ad57bead88b3fbd143f60ba
Roby Rodriguez <andrey.roba@gmail.com>
2018-05-10 11:34:20 +0300

    isolate firebase config

---
+++ client/public/firebase-config.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyCdHDx5jQ0v856rnvYaC5AHPsdhKRu27uU",
+    authDomain: "stack-buster.firebaseapp.com",
+    databaseURL: "https://stack-buster.firebaseio.com",
+    projectId: "stack-buster",
+    storageBucket: "stack-buster.appspot.com",
+    messagingSenderId: "575911053973"
+});
